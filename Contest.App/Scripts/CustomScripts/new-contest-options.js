35c88556cf5c7261b35d80c644fc01746db6cd5a
Radoslav Nikolov <radoslavmnikolov@gmail.com>
2015-10-30 16:39:23 +0200

    Add admin logic and contest input form

--- Contest.App/Scripts/CustomScripts/new-contest-options.js
+++ Contest.App/Scripts/CustomScripts/new-contest-options.js
@@ -3,76 +3,123 @@
         locale: 'en'
     });
 
-    $('#multiple-winners').click(function () {
-        var rewardType = $('#reward-type');
-        var multipleWinnerDiv = $('<div>')
-            .attr({ id: 'participants-section' })
-            .addClass('form-group')
-            .insertAfter(rewardType);
+    //$('#multiple-winners').click(function () {
+    //    var rewardType = $('#reward-type');
+    //    var multipleWinnerDiv = $('<div>')
+    //        .attr({ id: 'participants-section' })
+    //        .addClass('form-group')
+    //        .insertAfter(rewardType);
 
-        $('<label>')
-            .attr({ id: 'participants-number-label' })
-            .addClass('col-md-2 control-label')
-            .text('Winers number:')
-            .appendTo(multipleWinnerDiv);
+    //    $('<label>')
+    //        .attr({ id: 'participants-number-label' })
+    //        .addClass('col-md-2 control-label')
+    //        .text('Winers number:')
+    //        .appendTo(multipleWinnerDiv);
 
-        var inputDiv = $('<div>')
-            .addClass('col-md-10')
-            .appendTo(multipleWinnerDiv);
+    //    var inputDiv = $('<div>')
+    //        .addClass('col-md-10')
+    //        .appendTo(multipleWinnerDiv);
 
-        $('<input/>')
-            .attr({ type: 'number', min: 2, max: 10, name: 'participants', autofocus: true, id: 'participants-input' })
-            .addClass('form-control')
-            .appendTo(inputDiv);
-    });
+    //    $('<input/>')
+    //        .attr({ type: 'number', min: 2, max: 10, name: 'participants', autofocus: true, id: 'participants-input' })
+    //        .addClass('form-control')
+    //        .appendTo(inputDiv);
+    //});
 
-    $('#single-winner').click(function () {
-        $('#participants-section').remove();
-        $('#participants-number-label').remove();
-        $('#participants-input').remove();
-    });
+    //$('#single-winner').click(function () {
+    //    $('#participants-section').remove();
+    //    $('#participants-number-label').remove();
+    //    $('#participants-input').remove();
+    //});
 
-    $('#voting-close').click(function () {
-        $.get('/Users/GetAllUsers',
-            function (data) {
-                if (data) {
-                    var rewardType = $('#voting-type');
-                    var manyVotersDiv = $('<div>')
-                        .attr({ id: 'voters-section' })
-                        .addClass('form-group')
-                        .insertAfter(rewardType);
+    //$('#voting-close').click(function () {
 
-                    $('<label>')
-                        .attr({ id: 'participants-label' })
-                        .addClass('col-md-2 control-label')
-                        .text('Participants:')
-                        .appendTo(manyVotersDiv);
 
-                    var inputDiv = $('<div>')
-                        .addClass('col-md-10')
-                        .appendTo(manyVotersDiv);
+    //    $.get('/Users/GetAllUsers',
+    //        function (data) {
+    //            if (data) {
+    //                var rewardType = $('#voting-type');
+    //                var manyVotersDiv = $('<div>')
+    //                    .attr({ id: 'voters-section' })
+    //                    .addClass('form-group')
+    //                    .insertAfter(rewardType);
 
-                    var votersUl = $('<ul>')
-                        .addClass('nav nav-pills')
-                        .appendTo(inputDiv);
-                    data.forEach(function (user) {
-                        var list = $('<li>').addClass('block-item');
-                        $('<p>')
-                            .text(user.Username)
-                            .appendTo(list);
+    //                $('<label>')
+    //                    .attr({ id: 'participants-label' })
+    //                    .addClass('col-md-2 control-label')
+    //                    .text('Participants:')
+    //                    .appendTo(manyVotersDiv);
 
-                        //$('<img>')
-                        //    .attr('src', user.ProfileImage)
-                        //    .appendTo(list);
+    //                var inputDiv = $('<div>')
+    //                    .addClass('col-md-10')
+    //                    .appendTo(manyVotersDiv);
 
-                        list.appendTo(votersUl);
-                    });
-                }
-            });
 
-    });
 
-    $('#voting-open').click(function () {
-        $('#voters-section').remove();
-    });
+
+    //                var votersUl = $('<ul>')
+    //                    .addClass('nav nav-pills')
+    //                    .appendTo(inputDiv);
+    //                data.forEach(function (user) {
+    //                    var list = $('<li>').addClass('block-item');
+    //                    $('<p>')
+    //                        .text(user.Username)
+    //                        .appendTo(list);
+
+    //                    //$('<img>')
+    //                    //    .attr('src', user.ProfileImage)
+    //                    //    .appendTo(list);
+
+    //                    list.appendTo(votersUl);
+    //                });
+    //            }
+    //        });
+
+    //});
+
+    //$('#voting-open').click(function () {
+    //    $('#voters-section').remove();
+    //});
+
+    ////
+
+    //$(document).on('click', '#btnRight', function (e) {
+    //    var selectedOpts = $('#lstBox1 option:selected');
+    //    if (selectedOpts.length === 0) {
+    //        alert("Nothing to move.");
+    //        e.preventDefault();
+    //    }
+
+    //    $('#lstBox2').append($(selectedOpts).clone());
+    //    $(selectedOpts).remove();
+    //    e.preventDefault();
+    //});
+
+    //$(document).on('click', '#btnLeft', function (e) {
+    //    var selectedOpts = $('#lstBox2 option:selected');
+    //    if (selectedOpts.length === 0) {
+    //        alert("Nothing to move.");
+    //        e.preventDefault();
+    //    }
+
+    //    $('#lstBox1').append($(selectedOpts).clone());
+    //    $(selectedOpts).remove();
+    //    e.preventDefault();
+    //});
+
+    //function fillCategories() {
+    //    $.ajax({
+    //        type: "GET",
+    //        url: "/Users/GetAllUsers"
+    //    })
+    //        .done(function (data) {
+    //            $('#categories').html(data);
+    //        });
+    //};
+    //fillCategories();
+
+    ////
+
+
+
 });
