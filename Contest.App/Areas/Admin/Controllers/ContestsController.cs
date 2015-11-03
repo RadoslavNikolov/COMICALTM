38410205a5bb7292d9644f6769ff0c19697385d8
Nikola Nikolov <nikola_00219@abv.bg>
2015-11-03 19:28:33 +0200

    add error handling page

---
+++ Contest.App/Areas/Admin/Controllers/ContestsController.cs
@@ -0,0 +1,18 @@
+﻿namespace Contests.App.Areas.Admin.Controllers
+{
+    using System.Web.Mvc;
+    using Data.UnitOfWork;
+
+    public class ContestsController : BaseAdminController
+    {
+        public ContestsController(IContestsData data)
+            : base(data)
+        {
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
